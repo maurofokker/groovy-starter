f3a01c8b2aad70bdc1fe3030a2f4bd576ce0e954
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2018-03-21 13:25:34 -0300

    groovy scope and delegates in closures

--- src/cl/groovy/closures/Delegates.groovy
+++ src/cl/groovy/closures/Delegates.groovy
@@ -1,15 +1,26 @@
+package cl.groovy.closures
 // Delegates.groovy
-
+/*
+    Owner, delegate and this
+    ---------------------------
+    owner   : corresponds to the enclosing object where the closure is defined,
+            which may be either a class or a closure
+    delegate: corresponds to a third party object where methods calls or properties
+            are resolved whenever the receiver of the message is not defined
+    this    : corresponds to the enclosing class where the closure is defined
+ */
 def writer = {
-    append 'Dan'
-    append ' lives in Cleveland'
+    append 'John'
+    append ' lives in Santiago'
 }
 
 def append(String s) {
     println "append() called with argument of $s"
 }
 
+// to delegate to append method in StringBuffer that is another class
+// so not delegate in our append implementation
 StringBuffer sb = new StringBuffer()
-writer.resolveStrategy = Closure.DELEGATE_FIRST
+writer.resolveStrategy = Closure.DELEGATE_FIRST // without this line the strategy resolution will be our implementation of append method
 writer.delegate = sb
 writer()
--- src/cl/groovy/closures/Scope.groovy
+++ src/cl/groovy/closures/Scope.groovy
@@ -1,19 +1,22 @@
+package cl.groovy.closures
 /*
-  this corresponds to the enclosing class where the closure is defined
-  owner corresponds to the enclosing object where the closure is defined, which may be either a class or a closure
-  delegate corresponds to a third party object where methods calls or properties are resolved whenever the receiver of the message is not defined
+  this      : corresponds to the enclosing class where the closure is defined
+  owner     : corresponds to the enclosing object where the closure is defined,
+            which may be either a class or a closure
+  delegate  : corresponds to a third party object where methods calls or properties
+            are resolved whenever the receiver of the message is not defined
 */
 
 class ScopeDemo {
 
-    def outerClosure = {
-        println this.class.name
-        println owner.class.name
-        println delegate.class.name
-        def nestedClosure = {
-            println this.class.name
-            println owner.class.name
-            println delegate.class.name        
+    def outerClosure = {                // outer closure
+        println this.class.name     // print ScopeDemo
+        println owner.class.name    // print ScopeDemo
+        println delegate.class.name // print ScopeDemo  // most of the time owner and delegate are the same
+        def nestedClosure = {           // nested closure
+            println this.class.name         // print ScopeDemo
+            println owner.class.name        // print ScopeDemo$_closure1 (aka outerClosure)
+            println delegate.class.name     // print ScopeDemo$_closure1 (aka outerClosure)
         }
         nestedClosure()
     }
