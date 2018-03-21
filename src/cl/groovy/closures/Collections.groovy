f9baba95b050656a16c532e779d0a6fe7f11c528
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2018-03-21 11:48:16 -0300

    collections with closures

--- src/cl/groovy/closures/Collections.groovy
+++ src/cl/groovy/closures/Collections.groovy
@@ -1,23 +1,29 @@
+package cl.groovy.closures
 // each & eachWithIndex
 List favNums = [2,21,44,35,8,4]
 
+// for to print elements in list
 for(num in favNums) {
     println num
 }
 
+// each method accepting Closure
+// bc is just one param there is no need to use () in method
 favNums.each { println it }
 
+// for the old way to have indexes
 for( int i=0; i<favNums.size(); i++){
     println "$i:${favNums[i]}"
 }
 
+// eachWithIndex method with args
 favNums.eachWithIndex { num, idx ->
     println "$idx:$num"
 }
 
 // findAll
 List days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-List weekend = days.findAll { it.startsWith("S") }
+List weekend = days.findAll { it.startsWith("S") }  // closure will evaluate each element in the list
 
 println days
 println weekend
@@ -25,11 +31,15 @@ println weekend
 // collect
 List nums = [1,2,2,7,2,8,2,4,6]
 
+// this is not the more efficient way bc we are looping over a list to create a new list
 List numsTimesTen = []
 nums.each { num ->
     numsTimesTen << num * 10
 }
 
+// functional approach using collect method
+// this will collect the data and manipulate them
+// this will return a new list based in the manipulate of the original one
 List newTimesTen = nums.collect { num -> num * 10 }
 
 println nums
