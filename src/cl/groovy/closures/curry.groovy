5d2f23ca30e093b2f0076ec9cc58ad26d755e77a
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2018-03-21 12:58:30 -0300

    groovy curry in closures

--- src/cl/groovy/closures/curry.groovy
+++ src/cl/groovy/closures/curry.groovy
@@ -1,3 +1,7 @@
+package cl.groovy.closures
+
+// curry in groovy refers to the concept of partial application
+// curry will set the parameters of one method and will return a new Closure accepting one last parameter
 
 def log = { String type, Date createdOn, String msg -> 
     println "$createdOn [$type] - $msg"
@@ -5,17 +9,20 @@ def log = { String type, Date createdOn, String msg ->
 
 log("DEBUG",new Date(),"This is my first debug statement...")
 
+// Applying curry creating new closure depending in other one
+// the new method will receive just 2 args, bc in the declaration the type parameter is already used
 def debugLog = log.curry("DEBUG")
 debugLog(new Date(), "This is another debug statement...")
 debugLog(new Date(), "This is one more...")
 
+// Creating a new closure
 def todayDebugLog = log.curry("DEBUG",new Date())
 todayDebugLog("This is today's debug msg")
 
-// right curry
+// right curry will replace parameters from right to the left
 def crazyPersonLog = log.rcurry("Why am I logging this way")
 crazyPersonLog("ERROR",new Date())
 
-// index based currying
+// index based currying will replace parameter based on index
 def typeMsgLog = log.ncurry(1,new Date())
 typeMsgLog("ERROR","This is using ncurry...")
